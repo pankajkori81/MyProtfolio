1351e58b80e60c67f1b173ada804b41e0f437a98
Pankaj kori <93833439+tokio8169@users.noreply.github.com>
2024-05-31 01:34:54 +0530

    Add files via upload

---
+++ src/Mainapps.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+import {BrowserRouter , Router , Routes , Route , Link}  from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+
+import "./Mainapps.css";
+import Service from "./Pages/Service";
+import Connectwithme from "./Pages/Connectwithme";
+import Headerinfo from "./Components/Headerinfo";
+import Skills from "./Pages/Skills";
+import Project from "./Pages/Project";
+
+
+function Mainapps(){
+
+
+    return(
+        <div>
+
+          
+          <BrowserRouter>
+
+
+          <div className="logo">
+           <img src="logo_transparent.png" alt="" />
+         </div>
+
+         <div className="navbar">
+                <ul className="navbars">
+                <li><a  href="#"><Link className="nav1" to="/">Home</Link></a></li>
+                <li><a  href="#"><Link className="nav1" to="/about">About</Link></a></li>
+                <li><a  href="#"><Link className="nav1" to="/service">Service</Link></a></li>
+                <li><a  href="#"><Link className="nav1" to="/skills">Skills</Link></a></li>
+                <li><a  href="#"><Link className="nav1" to="/project">Projects</Link></a></li>
+                {/* <li><a  href="#"><Link className="nav2" to="/connectwithme">Connect With Me</Link></a></li>
+                 */}
+
+                <li><a  href="#"><Link className="nav2" to="/contact">Contact Us</Link></a></li>
+                </ul>
+           
+
+         </div>
+
+
+            
+
+          <Routes>
+            <Route  path="/" element Component={Headerinfo}/>
+            <Route  path="/about" element Component={About} />
+            <Route path="/service" element Component={Service}/>
+            <Route path="/contact" element Component={Contact} />
+            <Route path="/skills" element Component={Skills} />
+            <Route path="/project" element Component={Project} />
+ 
+
+            <Route path="/connectwithme" element Component={Connectwithme} />
+          </Routes>
+
+          
+        
+
+
+          
+          </BrowserRouter>
+
+        
+
+           
+
+        </div>
+    )
+};
+
+export default Mainapps;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Mainapps from './Mainapps';
+
+
+ReactDOM.render(<>
+
+<div>
+<Mainapps/>
+</div>
+</> , document.getElementById("root"));
+
