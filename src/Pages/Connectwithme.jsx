4bbdb0833b04fa2c15041f3be98c677db46ba7a1
Pankaj kori <93833439+tokio8169@users.noreply.github.com>
2024-06-01 20:49:19 +0530

    Add files via upload

---
+++ src/Pages/Connectwithme.jsx
@@ -0,0 +1,15 @@
+import React  from "react";
+
+
+
+function Connectwithme(){
+    return(
+
+        <div>
+            <h1> Connect with me</h1>
+
+        </div>
+    )
+};
+
+export default Connectwithme;
